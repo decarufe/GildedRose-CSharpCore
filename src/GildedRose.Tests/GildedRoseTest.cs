164694d4b691f94c1655bd0d15977f1733c69c50
Eric De Carufel <eric.decarufel@gmail.com>
2021-05-19 08:26:58 -0400

    Initial Commit

---
+++ src/GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Items[0].Should().BeEquivalentTo(new Item {Name = "fixme", SellIn = 0, Quality = 0});
+        }
+    }
+}
